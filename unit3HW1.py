c8189876c6287610c4fcfe7ceb4914c40193067f
Delaina Poppleton <poppletd@wlhs.wlwv.k12.or.us>
2024-10-09 09:46:27 -0700

    Update unit3HW1.py

--- unit3HW1.py
+++ unit3HW1.py
@@ -1,6 +1,45 @@
-# approach 1
+# approach 1, good results
 
-cat_age = input("enter the age of the cat in months: ")
+cat_age = int(input("enter the age of the cat in months: "))
 
-if cat_age >= 6:
-  print("kitten, $250")
+if cat_age <= 6:
+    print(f"The price of the kitten is $250")
+elif cat_age <= 11:
+    print(f"The price of the teenager cat is $225")
+elif cat_age <= 95:
+    print(f"The price of the adult cat is $150")
+elif cat_age >= 96:
+    print(f"The price of the senior cat is $85")
+
+# approach 2, not so great results 
+
+if :cat_age >= 96:
+    print(f"The price of the senior cat is $85")
+elif cat_age <= 95:
+    print(f"The price of the adult cat is $150")
+elif cat_age <= 11:
+    print(f"The price of the teenager cat is $225")
+elif cat_age <= 6:
+  print(f"The price of the kitten is $250")
+
+# approach 3, not so great results
+
+if cat_age <= 6:
+    print(f"The price of the kitten is $250")
+if cat_age <= 11:
+    print(f"The price of the teenager cat is $225")
+if cat_age <= 95:
+    print(f"The price of the adult cat is $150")
+if cat_age >= 96:
+    print(f"The price of the senior cat is $85")
+
+# approach 4, not so great results
+
+if :cat_age >= 96:
+    print(f"The price of the senior cat is $85")
+if cat_age <= 95:
+    print(f"The price of the adult cat is $150")
+if cat_age <= 11:
+    print(f"The price of the teenager cat is $225")
+if cat_age <= 6:
+  print(f"The price of the kitten is $250")
