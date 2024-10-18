95cd3a6e35aa916e7797e6c8d629faab63a1cb48
Delaina Poppleton <poppletd@wlhs.wlwv.k12.or.us>
2024-10-18 09:14:17 -0700

    Update unit3quiz.py

--- unit3quiz.py
+++ unit3quiz.py
@@ -1,2 +1,14 @@
 word = input("enter a word: ")
-if word 
+word_length = len(word)
+if word_length < 4:
+    score = 1
+    print(f"score: {score}")
+elif 4 <= word_length <6:
+    score = 3
+    print(f"score: {score}")
+elif 6 <= word_length < 8:
+    score = 4
+    print(f"score: {score}")
+else:
+    score = 10
+    print(f"score: {score}")
